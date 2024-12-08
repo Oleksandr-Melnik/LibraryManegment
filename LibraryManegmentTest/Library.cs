78240a5d76c99b68569b7bd1391be1c9af5ca05d
Oleksandr-Melnik <oleksandr2428@gmail.com>
2024-12-08 14:36:19 +0200

    changed the file

--- LibraryManegmentTest/Library.cs
+++ LibraryManegmentTest/Library.cs
@@ -13,15 +13,18 @@ namespace LibraryManegmentTest
         public void RemoveBook(string isbn)
         {
             var book = books.FirstOrDefault(b => b.ISBN == isbn);
+            // Якщо книга не знайдена, кидаємо помилку
             if (book == null)
+
                 throw new InvalidOperationException("Book not found.");
+
             books.Remove(book);
         }
 
 
         public void AddBook(Book book)
         {
-            if (books.Any(b => b.ISBN == book.ISBN))
+            if (books.Any(b => b.ISBN == book.ISBN)) // Перевірка на унікальність ISBN
                 throw new InvalidOperationException("Book with the same ISBN already exists.");
             books.Add(book);
         }
